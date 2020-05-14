4e6182994918c295816b414e1c2cda683d1337e1
KKambi <37494867+KKambi@users.noreply.github.com>
2020-05-14 22:34:58 +0900

    Refactor: 컴포넌트를 불러오는 구조 변경

---
+++ client/src/components/App.mjs
@@ -0,0 +1,60 @@
+import SearchBar from "./SerachBar/SearchBar.mjs";
+import Grid from "./Grid/Grid.mjs";
+
+export default class App {
+  root = null;
+  test = 3;
+
+  constructor() {
+    //루트
+    this.root = document.querySelector("#root");
+
+    this.render();
+  }
+
+  setTheme() {
+    const userTheme = localStorage.getItem("userTheme");
+    const body = document.querySelector("body");
+
+    //1. 토글한 적이 있다면 기존 모드대로 설정
+    if (userTheme) {
+      body.className = userTheme;
+    } else {
+      //2. 토글한 적이 없다면, OS 선호 모드가 있는지 확인
+      const mqList = window.matchMedia("@media (prefers-color-scheme: dark)");
+      if (mqList.matches) {
+        body.className = "dark";
+      } else {
+        //3. 선호 모드가 없다면 기본 light 모드
+        body.className = "light";
+      }
+    }
+  }
+
+  render() {
+    //모드 설정
+    this.setTheme();
+
+    //상단 검색바 렌더링
+    const serachBar = new SearchBar({
+      target: this.root,
+      className: "search-bar",
+    });
+
+    //그리드 테스트 데이터
+    const gridTestData = [];
+    for (let i = 0; i <= 17; i++) {
+      const imageObject = {
+        name: `grid-${i}`,
+        url: `https://picsum.photos/600/400/?random?${i}`,
+      };
+      gridTestData.push(imageObject);
+    }
+
+    //그리드 렌더링
+    const photoGrid = new Grid({
+      target: this.root,
+      data: gridTestData,
+    });
+  }
+}
--- client/src/javascripts/index.mjs
+++ client/src/javascripts/index.mjs
@@ -1,16 +1,3 @@
-import Grid from "../components/Grid.mjs";
+import App from "../components/App.mjs";
 
-(() => {
-  const root = document.querySelector("#root");
-
-  const gridTestData = [];
-  for (let i = 0; i <= 17; i++) {
-    const imageObject = {
-      name: `grid-${i}`,
-      url: `http://localhost:3000/images/(${i}).jpg`,
-    };
-    gridTestData.push(imageObject);
-  }
-
-  const grid = new Grid(root, gridTestData);
-})();
+new App();
