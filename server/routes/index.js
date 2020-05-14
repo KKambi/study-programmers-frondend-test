72fbf4b539266e83de4ceb718e3c1c83e2423131
KKambi <37494867+KKambi@users.noreply.github.com>
2020-05-14 23:45:19 +0900

    Add: 이미지를 제공하는 임시 API

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const fs = require("fs");
+const path = require("path");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/image/:id", (req, res, next) => {
+  const { id } = req.params;
+
+  //파일 존재 여부 체크
+  try {
+    const filePath = path.resolve(__dirname, `../public/images/(${id}).jpg`);
+    fs.statSync(filePath);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      res.status(404).json({ error: "파일 존재하지 않음" });
+      return;
+    }
+  }
+
+  const response = {
+    name: `image-${id}`,
+    url: `http://localhost:3000/images/(${id})`,
+  };
+
+  res.status(200).json(response);
 });
 
 module.exports = router;
